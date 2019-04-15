fb39ee59c1566bb4750e99d506a9c0eeaf50d12d
Madison Cooper <madisondcooper@gmail.com>
2019-04-15 04:05:41 +0000

    validation, making sure the quotes is not too long or short

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -11,6 +11,8 @@ class QuotesController < ApplicationController
     @quote = Quote.create(quote_params)
     if @quote.invalid?
       flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
   end
 
   private
